a3b4c005e3bde21130bfe3c7bd6d96c18fdc72ea
malathiganesh <107910492+malathiganesh@users.noreply.github.com>
2022-09-09 14:55:37 +0530

    id-practice

---
+++ id1.js
@@ -0,0 +1,26 @@
+
+
+function myFun(){
+    var a = document.getElementById("id1");
+    a.style.color="red";
+}
+// var a=document.getElementById("id1").innerHTML="helllo im here";
+// console.log(a);
+// document.write(a);
+
+// var a=document.getElementById("id1").innerHTML="heloooo there";
+// console.log(a);
+// function myDis(some){
+//     document.getElementById("id1").innerHTML=some;
+// }
+// function myFirst(){
+//     myDis("heloooo");
+// }
+// function meSecond(){
+//     myDis('im here');
+// }
+// myFirst()
+// meSecond()
+// var a=[1,2,3,4];
+// let arr=a.map(x=>x*2);
+// console.log(arr);
